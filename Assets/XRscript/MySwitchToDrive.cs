40bc9a16b8c72b61088a52e801cdb0ed4400895c
LeeJohnn <bello44691@gmail.com>
2021-03-27 21:12:02 +0800

    Fixed Scene2 script

---
+++ Assets/XRscript/MySwitchToDrive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySwitchToDrive : MonoBehaviour
+{
+    public GameObject WalkingDad;
+    public GameObject DrivingDad;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("NPC")){
+            WalkingDad.SetActive(false);
+            DrivingDad.SetActive(true);
+        }
+    }
+}
